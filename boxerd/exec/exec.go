f4fe065ba21d566288ae70d1aaa6a182a5171078
hongsam14 <ghdtjdgus14@gmail.com>
2025-01-28 01:51:06 +0900

    Dev: dev promise object

--- boxerd/exec/exec.go
+++ boxerd/exec/exec.go
@@ -1,10 +1,51 @@
 package exec
 
 import (
-	"sync"
+	berror "boxerd/error"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+
+	"golang.org/x/sync/errgroup"
 )
 
-type Promise[t any] struct {
-	wait  sync.WaitGroup
-	value t
+type Promise struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+	cmd    exec.Cmd
+	eg     *errgroup.Group
+}
+
+func Run(inStream *os.File, outStream *os.File, arg0 string, args ...string) (promise *Promise) {
+	promise = new(Promise)
+	promise.ctx, promise.cancel = context.WithCancel(context.Background())
+	// set the commandline
+	promise.cmd = *exec.CommandContext(promise.ctx, arg0, args...)
+	promise.cmd.Stdin = inStream
+	promise.cmd.Stdout = outStream
+	promise.cmd.Stderr = outStream
+
+	// set the error for run commandline goroutine
+	promise.eg, _ = errgroup.WithContext(promise.ctx)
+	promise.eg.Go(func() error {
+		return promise.cmd.Run()
+	})
+	return promise
+}
+
+func (p *Promise) Wait() (err error) {
+	err = p.eg.Wait()
+	if err != nil {
+		return berror.BoxerError{
+			Code:   berror.InvalidOperation,
+			Msg:    fmt.Sprintf("error while execute %s %v promise.Wait()", p.cmd.Path, p.cmd.Args),
+			Origin: err,
+		}
+	}
+	return nil
+}
+
+func (p *Promise) Cancel() {
+	p.cancel()
 }
--- boxerd/exec/go.mod
+++ boxerd/exec/go.mod
@@ -1,3 +1,5 @@
 module boxerd/exec
 
 go 1.23.3
+
+require golang.org/x/sync v0.6.0
