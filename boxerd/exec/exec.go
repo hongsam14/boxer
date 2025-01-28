97538ed4fd15850a44e550fc670ff644f83e0b7c
hongsam14 <ghdtjdgus14@gmail.com>
2025-01-28 18:39:26 +0900

    Dev: guard duplicated function call

--- boxerd/exec/exec.go
+++ boxerd/exec/exec.go
@@ -6,20 +6,24 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sync/atomic"
 
 	"golang.org/x/sync/errgroup"
 )
 
 type Promise struct {
-	ctx    context.Context
-	cancel context.CancelFunc
-	cmd    exec.Cmd
-	eg     *errgroup.Group
+	ctx     context.Context
+	cancel  context.CancelFunc
+	cmd     exec.Cmd
+	eg      *errgroup.Group
+	waitCnt int32
 }
 
 func Run(inStream *os.File, outStream *os.File, arg0 string, args ...string) (promise *Promise) {
 	promise = new(Promise)
 	promise.ctx, promise.cancel = context.WithCancel(context.Background())
+	// set conditional variable to -1
+	atomic.StoreInt32(&promise.waitCnt, -1)
 	// set the commandline
 	promise.cmd = *exec.CommandContext(promise.ctx, arg0, args...)
 	promise.cmd.Stdin = inStream
@@ -31,10 +35,31 @@ func Run(inStream *os.File, outStream *os.File, arg0 string, args ...string) (pr
 	promise.eg.Go(func() error {
 		return promise.cmd.Run()
 	})
+	// set the conditional variable to 0
+	atomic.StoreInt32(&promise.waitCnt, 0)
 	return promise
 }
 
 func (p *Promise) Wait() (err error) {
+	// if errgroup is not initialized, return error
+	if p.eg == nil {
+		return berror.BoxerError{
+			Code:   berror.InvalidOperation,
+			Origin: fmt.Errorf("promise is not initialized"),
+			Msg:    "error while execute promise.Wait()",
+		}
+	}
+	// if conditional variable is not set to 0, return error
+	if atomic.LoadInt32(&p.waitCnt) != 0 {
+		return berror.BoxerError{
+			Code:   berror.InvalidOperation,
+			Msg:    fmt.Sprintf("error while execute %s %v promise.Wait()", p.cmd.Path, p.cmd.Args),
+			Origin: fmt.Errorf("promise is not initialized [%d]", atomic.LoadInt32(&p.waitCnt)),
+		}
+	}
+	// set the conditional variable to 1
+	atomic.StoreInt32(&p.waitCnt, 1)
+	// wait for the errgroup to finish
 	err = p.eg.Wait()
 	if err != nil {
 		return berror.BoxerError{
@@ -46,6 +71,22 @@ func (p *Promise) Wait() (err error) {
 	return nil
 }
 
-func (p *Promise) Cancel() {
+func (p *Promise) Cancel() (err error) {
+	if p.cancel == nil {
+		return berror.BoxerError{
+			Code:   berror.InvalidOperation,
+			Msg:    "error while execute promise.Cancel()",
+			Origin: fmt.Errorf("promise is not initialized"),
+		}
+	}
+	if atomic.LoadInt32(&p.waitCnt) != 0 {
+		return berror.BoxerError{
+			Code:   berror.InvalidOperation,
+			Msg:    fmt.Sprintf("error while execute %s %v promise.Cancel()", p.cmd.Path, p.cmd.Args),
+			Origin: fmt.Errorf("promise is not initialized [%d]", atomic.LoadInt32(&p.waitCnt)),
+		}
+	}
+	// cancel the context
 	p.cancel()
+	return nil
 }
--- boxerd/exec/exec_test.go
+++ boxerd/exec/exec_test.go
@@ -42,3 +42,34 @@ func TestPromiseWithoutWait(t *testing.T) {
 		t.Errorf("Promise should not have waited for the command to finish")
 	}
 }
+
+func TestWaitBeforePromise(t *testing.T) {
+	promise := new(exec.Promise)
+	err := promise.Wait()
+	if err == nil {
+		t.Errorf("Error should have been raised")
+	}
+	t.Logf("Error: %v", err)
+}
+
+func TestCancelBeforePromise(t *testing.T) {
+	promise := new(exec.Promise)
+	err := promise.Cancel()
+	if err == nil {
+		t.Errorf("Error should have been raised")
+	}
+	t.Logf("Error: %v", err)
+}
+
+func TestWaitFuncCallDuplicated(t *testing.T) {
+	promise := exec.Run(os.Stdin, os.Stdout, "echo", "hello")
+	err := promise.Wait()
+	if err != nil {
+		t.Errorf("Error while executing promise %v", err)
+	}
+	err = promise.Wait()
+	if err == nil {
+		t.Errorf("Error should have been raised")
+	}
+	t.Logf("Error: %v", err)
+}
