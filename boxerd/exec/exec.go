930b4eee4b069faa08c608f1030ec3f349489596
hongsam14 <ghdtjdgus14@gmail.com>
2025-06-07 23:08:12 +0900

    Task: Improve exec & prevent zombie process

--- boxerd/exec/exec.go
+++ boxerd/exec/exec.go
@@ -7,86 +7,168 @@ import (
 	"os"
 	"os/exec"
 	"sync/atomic"
+	"syscall"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
-type Promise struct {
-	ctx     context.Context
-	cancel  context.CancelFunc
+// # Promise
+//
+// Promise calls an external program as a subprocess and manages it asynchronously.
+// It provides a way to wait for the subprocess to finish or cancel it.
+type Promise interface {
+	Pid() int
+	Wait() (int, error)
+	Cancel() error
+}
+
+type promise struct {
 	cmd     exec.Cmd
 	eg      *errgroup.Group
 	waitCnt int32
 }
 
-func Run(inStream *os.File, outStream *os.File, arg0 string, args ...string) (promise *Promise) {
-	promise = new(Promise)
-	promise.ctx, promise.cancel = context.WithCancel(context.Background())
-	// set conditional variable to -1
-	atomic.StoreInt32(&promise.waitCnt, -1)
-	// set the commandline
-	promise.cmd = *exec.CommandContext(promise.ctx, arg0, args...)
-	promise.cmd.Stdin = inStream
-	promise.cmd.Stdout = outStream
-	promise.cmd.Stderr = outStream
-
-	// set the error for run commandline goroutine
-	promise.eg, _ = errgroup.WithContext(promise.ctx)
-	promise.eg.Go(func() error {
-		return promise.cmd.Run()
-	})
-	// set the conditional variable to 0
-	atomic.StoreInt32(&promise.waitCnt, 0)
-	return promise
+func (p *promise) Pid() int {
+	return p.cmd.Process.Pid
 }
 
-func (p *Promise) Wait() (err error) {
-	// if errgroup is not initialized, return error
-	if p.eg == nil {
-		return berror.BoxerError{
-			Code:   berror.InvalidOperation,
-			Origin: fmt.Errorf("promise is not initialized"),
-			Msg:    "error while execute promise.Wait()",
+// # Run
+//
+// Run starts a new subprocess with the given commandline and returns a Promise.
+func Run(inStream *os.File, outStream *os.File, arg0 string, args ...string) (Promise, error) {
+	prom := new(promise)
+	// set conditional variable to -1
+	atomic.StoreInt32(&prom.waitCnt, -1)
+	// set the commandline
+	prom.cmd = *exec.Command(arg0, args...)
+	prom.cmd.Stdin = inStream
+	prom.cmd.Stdout = outStream
+	prom.cmd.Stderr = os.Stderr
+
+	// generate process group
+	prom.cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+
+	// set the error for run commandline goroutine
+	err := prom.cmd.Start()
+	if err != nil {
+		return nil, berror.BoxerError{
+			Code:   berror.SystemError,
+			Origin: err,
+			Msg:    fmt.Sprintf("error while execute %s %v promise.Start()", prom.cmd.Path, prom.cmd.Args),
 		}
 	}
+	// set the conditional variable to 0
+	atomic.StoreInt32(&prom.waitCnt, 0)
+	return prom, nil
+}
+
+// # Wait
+//
+// Wait waits for the subprocess to finish and returns an error if the subprocess is not successful.
+// Wait should be called only once and it will return an error if it is called multiple times.
+// Also, it will return an error if it is called before Run.
+// ExitCode will be returned as 0 if the subprocess is successful.
+// Also ExitCode will be returned as -1 if subprocess is killed by signal.
+// So the caller should check the ExitCode and Error to determine if the subprocess is successful or not.
+// Because ExitCode -1 means the subprocess is killed by signal, and it is not an error.
+func (p *promise) Wait() (exitCode int, err error) {
 	// if conditional variable is not set to 0, return error
-	if atomic.LoadInt32(&p.waitCnt) != 0 {
-		return berror.BoxerError{
+	val := atomic.LoadInt32(&p.waitCnt)
+	if val != 0 {
+		// errorcode 1 means general error
+		return 1, berror.BoxerError{
 			Code:   berror.InvalidOperation,
 			Msg:    fmt.Sprintf("error while execute %s %v promise.Wait()", p.cmd.Path, p.cmd.Args),
-			Origin: fmt.Errorf("promise is not initialized [%d]", atomic.LoadInt32(&p.waitCnt)),
+			Origin: fmt.Errorf("wait is called while promise is not initialized [%d]", atomic.LoadInt32(&p.waitCnt)),
 		}
 	}
 	// set the conditional variable to 1
 	atomic.StoreInt32(&p.waitCnt, 1)
-	// wait for the errgroup to finish
+	state, err := p.cmd.Process.Wait()
+	if err != nil {
+		// errorcode 130 means fatal error
+		return 130, berror.BoxerError{
+			Code:   berror.SystemError,
+			Msg:    fmt.Sprintf("error while execute %s %v promise.Wait()", p.cmd.Path, p.cmd.Args),
+			Origin: err,
+		}
+	}
+	exitCode = state.ExitCode()
+	// release the process resource to prevent zombie process
+	err = p.cmd.Process.Release()
+	if exitCode < 0 && err != nil {
+		return exitCode, berror.BoxerError{
+			Code:   berror.SystemError,
+			Msg:    fmt.Sprintf("error while execute %s %v promise.Wait() in Process.Release()", p.cmd.Path, p.cmd.Args),
+			Origin: err,
+		}
+	}
+	return exitCode, nil
+}
+
+// # Cancel
+//
+// Cancel sends a signal to the subprocess to kill it.
+// It will return an error if the subprocess is not initialized.
+// It will be blocked until the subprocess is killed and release the resource.
+func (p *promise) Cancel() (err error) {
+	if atomic.LoadInt32(&p.waitCnt) < 0 {
+		return berror.BoxerError{
+			Code:   berror.InvalidOperation,
+			Msg:    fmt.Sprintf("error while execute %s %v promise.Cancel()", p.cmd.Path, p.cmd.Args),
+			Origin: fmt.Errorf("cancel is called while promise is not initialized [%d]", atomic.LoadInt32(&p.waitCnt)),
+		}
+	}
+	// create timeout context for killing subprocesskiller with timeout.
+	// if the subprocesskiller is not killed in the timeout, the subprocesskiller will be killed by the parent process.
+	subProcessKillerCtx, subProcessKillerCancel := context.WithDeadline(context.TODO(), time.Now().Add(5*time.Second))
+	defer subProcessKillerCancel()
+	p.eg, _ = errgroup.WithContext(subProcessKillerCtx)
+	// send signal to subprocesskiller
+	p.eg.Go(func() error {
+		return p.subProcessKiller()
+	})
+
 	err = p.eg.Wait()
 	if err != nil {
 		return berror.BoxerError{
-			Code:   berror.InvalidOperation,
-			Msg:    fmt.Sprintf("error while execute %s %v promise.Wait()", p.cmd.Path, p.cmd.Args),
+			Code:   berror.SystemError,
+			Msg:    fmt.Sprintf("error while execute %s %v promise.Cancel()", p.cmd.Path, p.cmd.Args),
 			Origin: err,
 		}
 	}
 	return nil
 }
 
-func (p *Promise) Cancel() (err error) {
-	if p.cancel == nil {
-		return berror.BoxerError{
-			Code:   berror.InvalidOperation,
-			Msg:    "error while execute promise.Cancel()",
-			Origin: fmt.Errorf("promise is not initialized"),
-		}
-	}
-	if atomic.LoadInt32(&p.waitCnt) != 0 {
-		return berror.BoxerError{
-			Code:   berror.InvalidOperation,
-			Msg:    fmt.Sprintf("error while execute %s %v promise.Cancel()", p.cmd.Path, p.cmd.Args),
-			Origin: fmt.Errorf("promise is not initialized [%d]", atomic.LoadInt32(&p.waitCnt)),
-		}
-	}
-	// cancel the context
-	p.cancel()
-	return nil
+func (p *promise) subProcessKiller() error {
+	// kill proc group
+	//
+	// (DEPRECATED) kill process group
+	//
+	// pgid, err := syscall.Getpgid(p.cmd.Process.Pid)
+	// if err == nil {
+	// 	fmt.Fprintf(os.Stderr, "Killing process group %d\n", pgid)
+	// 	syscall.Kill(-pgid, syscall.SIGKILL)
+	// }
+	//
+	// kill process by SIGKILL signal
+	//
+	// p.cmd.Process.Kill()
+
+	p.cmd.Process.Signal(syscall.SIGINT)
+	return p.cmd.Process.Signal(syscall.SIGTERM)
+
+	// wait to prevent zombie process (DEPRECATED)
+	// p.cmd.Wait()
+	// err := p.cmd.Process.Release()
+	// if err != nil {
+	// 	return perror.PolvoGeneralError{
+	// 		Code:   perror.SystemError,
+	// 		Msg:    fmt.Sprintf("error while execute %s %v in subProcessKiller", p.cmd.Path, p.cmd.Args),
+	// 		Origin: err,
+	// 	}
+	// }
+	// release the process resource
+	// return nil
 }
